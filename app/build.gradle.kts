acc3cf91d2c667f9a9c699173c07b964ff2c7b7a
alireza <alireza@nativeblocks.io>
2025-06-04 16:18:51 +0400

    add nativeblocks dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,26 @@
+import org.jetbrains.kotlin.konan.properties.Properties
+import java.io.FileInputStream
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.ksp)
+    alias(libs.plugins.nativeblocks)
 }
 
+val nativeblocksProps = Properties().apply {
+    load(FileInputStream(File(rootProject.rootDir, "nativeblocks.properties")))
+}
+
+val appId = "io.nativeblocks.template.android"
+
 android {
-    namespace = "io.nativeblocks.template.android"
+    namespace = appId
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "io.nativeblocks.template.android"
+        applicationId = appId
         minSdk = 26
         targetSdk = 35
         versionCode = 1
@@ -36,6 +47,19 @@ android {
     }
 }
 
+nativeblocks {
+    endpoint = nativeblocksProps["endpoint"] as String
+    authToken = nativeblocksProps["authToken"] as String
+    organizationId = nativeblocksProps["organizationId"] as String
+    basePackageName = appId
+    moduleName = "MyApp"
+}
+
+ksp {
+    arg("basePackageName", appId)
+    arg("moduleName", "MyApp")
+}
+
 dependencies {
 
     implementation(libs.androidx.core.ktx)
@@ -46,6 +70,13 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    implementation(libs.nativeblocks.android)
+    implementation(libs.nativeblocks.foundation.android)
+    implementation(libs.nativeblocks.wandkit.android)
+    implementation(libs.nativeblocks.compiler.android)
+    ksp(libs.nativeblocks.compiler.android)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
